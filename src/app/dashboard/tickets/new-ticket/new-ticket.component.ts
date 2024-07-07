0923d5e5a9ae3f5284ba1de9c4a3f12cdf5a076e
rutv1k46 <rutviksolanki01@gmail.com>
2024-07-07 13:53:23 -0400

    control component

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
-  selector: 'app-new-ticket',
-  standalone: true,
-  imports: [],
-  templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+    selector: 'app-new-ticket',
+    standalone: true,
+    templateUrl: './new-ticket.component.html',
+    styleUrl: './new-ticket.component.css',
+    imports: [ControlComponent]
 })
 export class NewTicketComponent {
 
